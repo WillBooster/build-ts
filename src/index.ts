ddc9a9305fa6a619eecc28ec796c2e21c6298c94
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-11-15 23:12:47 +0900

    .

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,10 @@
 import yargs from 'yargs';
 import { hideBin } from 'yargs/helpers';
 
-import { appBuilder } from './commands/appBuilder';
+import { appBuilder } from './commands/appBuilder.js';
 
 export async function cli(): Promise<void> {
   await yargs(hideBin(process.argv)).command(appBuilder).demandCommand().help().argv;
 }
+
+cli().then();
