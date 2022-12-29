786ad19d024ee54e15e14adb18f5e70e8087f1f5
Sakamoto, Kazunori <exkazuu@gmail.com>
2022-12-29 13:34:55 +0900

    fix: don't inline environment variable if undefined

--- src/commands/build.ts
+++ src/commands/build.ts
@@ -201,11 +201,15 @@ function loadEnvironmentVariables(
 ): Record<string, string> {
   const envVars: Record<string, string> = {};
   for (const name of (argv.env ?? []).map((e) => e.toString())) {
+    if (process.env[name] === undefined) continue;
+
     envVars[`process.env.${name}`] = JSON.stringify(process.env[name]);
   }
   for (const dotenvPath of argv.dotenv ?? []) {
     const parsed = dotenv.config({ path: dotenvPath.toString() }).parsed || {};
     for (const [key, value] of Object.entries(parsed)) {
+      if (value === undefined) continue;
+
       envVars[`process.env.${key}`] = JSON.stringify(value);
     }
   }
