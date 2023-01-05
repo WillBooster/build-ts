a41417fa7b947bdf8f53325572b0388505854192
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-01-05 23:53:34 +0900

    .

--- src/commands/build/build.ts
+++ src/commands/build/build.ts
@@ -76,7 +76,7 @@ export const build: CommandModule<unknown, InferredOptionTypes<typeof builder>> 
     let buildFailed = false;
     try {
       const _bundle = await rollup({
-        input: argv.input || path.join('src', 'index.ts'),
+        input: path.join(packageDirPath, argv.input || path.join('src', 'index.ts')),
         plugins,
       });
       bundle = _bundle;
