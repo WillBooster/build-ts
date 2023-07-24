e7808951ba1ac60fb5153ff5d0497bbb5eef9d96
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-07-24 15:22:13 +0900

    feat!: cascade -> cascade-env, node-env -> cascade-node-env

--- src/preprocessBuilder.ts
+++ src/preprocessBuilder.ts
@@ -3,12 +3,12 @@ export const preprocessBuilder = {
     description: '.env files to be loaded.',
     type: 'array',
   },
-  cascade: {
+  'cascade-env': {
     description:
       'environment to load cascading .env files (e.g., `.env`, `.env.<environment>`, `.env.local` and `.env.<environment>.local`)',
     type: 'string',
   },
-  'node-env': {
+  'cascade-node-env': {
     description:
       'environment to load cascading .env files (e.g., `.env`, `.env.<NODE_ENV>`, `.env.local` and `.env.<NODE_ENV>.local`). Preferred over `cascade`.',
     type: 'boolean',
