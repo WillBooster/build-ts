934ee21686c83ef6750ed3590a20f56669df94a6
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-08-12 15:29:21 +0900

    test: enhance tests

---
+++ test-fixtures/env.ts
@@ -0,0 +1,1 @@
+process.stdout.write(process.env.A || '0');
---
+++ tests/run.test.ts
@@ -0,0 +1,33 @@
+import { spawnAsync } from '@willbooster/shared-lib-node';
+import { describe, expect, it } from 'vitest';
+
+describe(
+  'run',
+  () => {
+    it.concurrent.each([
+      ['yarn start-prod run test-fixtures/env.ts', '0'],
+      ['yarn start-prod run test-fixtures/env.ts --env .env', '1'],
+      ['yarn start-prod run test-fixtures/env.ts --env=.env', '1'],
+      ['yarn start-prod run test-fixtures/env.ts --cascade-env ""', '1'],
+      ['yarn start-prod run test-fixtures/env.ts --cascade-env=""', '1'],
+      ['yarn start-prod run test-fixtures/env.ts --cascade-node-env', '1'],
+      ['yarn start-prod run --cascade-env="" test-fixtures/env.ts', '1'],
+      ['yarn start-prod run --cascade-node-env test-fixtures/env.ts', '1'],
+      ['yarn start-prod --cascade-env="" run test-fixtures/env.ts', '1'],
+      ['yarn start-prod --cascade-node-env run test-fixtures/env.ts', '1'],
+      // Options with a non-empty argument must be after positional arguments.
+      // ['yarn start-prod run --env .env test-fixtures/env.ts', '1'],
+      // ['yarn start-prod run --env=.env test-fixtures/env.ts', '1'],
+      // ['yarn start-prod run --cascade-env "" test-fixtures/env.ts', '1'],
+      // ['yarn start-prod --env .env run test-fixtures/env.ts', '1'],
+      // ['yarn start-prod --env=.env run test-fixtures/env.ts', '1'],
+      // ['yarn start-prod --cascade-env "" run test-fixtures/env.ts', '1'],
+    ])('%s', async (commandWithArgs, expectedStdout) => {
+      const [command, ...args] = commandWithArgs.split(' ');
+      const execRet = await spawnAsync(command, args);
+      expect(execRet.stdout.trim().split('\n').at(-1)?.trim()).toBe(expectedStdout);
+      expect(execRet.status).toBe(0);
+    });
+  },
+  { timeout: 60_000 }
+);
