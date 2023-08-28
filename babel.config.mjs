289b97ff2ac6800f920103ae0420ba605f0a1b61
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-08-28 14:48:36 +0900

    feat: support TC39 proposals

--- babel.config.mjs
+++ babel.config.mjs
@@ -4,6 +4,7 @@ import url from 'node:url';
 
 /** @type {import('@babel/core').TransformOptions} */
 const config = {
+  assumptions: {},
   presets: [
     [
       '@babel/preset-env',
@@ -16,7 +17,17 @@ const config = {
   plugins: [
     '@babel/proposal-class-properties',
     '@babel/proposal-numeric-separator',
-    '@babel/plugin-syntax-import-assertions',
+    // cf. https://babel.dev/blog/2023/05/26/7.22.0#explicit-resource-management-15633-15520
+    '@babel/plugin-proposal-explicit-resource-management',
+    // cf. https://babel.dev/blog/2023/05/26/7.22.0#import-attributes-15536-15620
+    '@babel/plugin-syntax-import-attributes',
+    // cf. https://babel.dev/blog/2023/05/26/7.22.0#decorators-updates-15570
+    [
+      '@babel/plugin-proposal-decorators',
+      {
+        version: '2023-05',
+      },
+    ],
   ],
   env: {
     production: {
@@ -50,18 +61,20 @@ const config = {
   },
 };
 
-if (process.env.BUILD_TS_COREJS) {
+if (process.env.BUILD_TS_COREJS || process.env.BUILD_TS_COREJS_WITH_PROPOSALS) {
   const rootPath = url.fileURLToPath(path.dirname(import.meta.url));
   const packageJson = JSON.parse(fs.readFileSync(path.join(rootPath, 'package.json'), 'utf8'));
   const [major, minor] = packageJson.dependencies['core-js'].split('.');
+  const proposals = process.env.BUILD_TS_COREJS_WITH_PROPOSALS ? { proposals: true } : {};
 
   if (process.env.BUILD_TS_TARGET_CATEGORY === 'app') {
     /** @type {import('@babel/core').PluginItem} */
     const presetEnvConfig = config.presets[0][1];
     presetEnvConfig.useBuiltIns = 'usage';
-    presetEnvConfig.corejs = `${major}.${minor}`;
+    presetEnvConfig.corejs = { version: `${major}.${minor}`, ...proposals };
   } else if (process.env.BUILD_TS_TARGET_CATEGORY === 'lib') {
-    config.plugins.push(['polyfill-corejs3', { method: 'usage-pure', version: `${major}.${minor}` }]);
+    // cf. https://github.com/babel/babel-polyfills#injection-methods
+    config.plugins.push(['polyfill-corejs3', { method: 'usage-pure', version: `${major}.${minor}`, ...proposals }]);
   }
 }
 
