758780906c4a0aef21d152f708ccff95602db1ee
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2023-09-03 10:15:43 +0900

    feat!: restructure output directories (#132)

--- test-fixtures/lib/src/index.ts
+++ test-fixtures/lib/src/index.ts
@@ -3,3 +3,5 @@ import assert from 'node:assert';
 import chunk from 'lodash/chunk';
 
 assert(JSON.stringify(chunk(['a', 'b', 'c', 'd'], 2)) === '[["a","b"],["c","d"]]');
+
+export { add } from './module';
