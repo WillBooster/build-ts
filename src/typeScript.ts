946ffcbcd6bda8a93041ad9f20d3b00177f84771
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-09-04 22:52:19 +0900

    feat: support TypeScript 5.2.2

---
+++ src/typeScript.ts
@@ -0,0 +1,36 @@
+import ts from 'typescript';
+
+export function generateDeclarationFiles(projectDirPath: string): void {
+  const configFile = ts.findConfigFile(projectDirPath, ts.sys.fileExists);
+  if (!configFile) throw new Error('Failed to find tsconfig.json.');
+
+  const { config } = ts.readConfigFile(configFile, ts.sys.readFile);
+  config.compilerOptions = {
+    ...config.compilerOptions,
+    declaration: true,
+    emitDeclarationOnly: true,
+    noEmit: false,
+    noEmitOnError: true,
+    outDir: 'dist',
+  };
+
+  const { errors, fileNames, options } = ts.parseJsonConfigFileContent(config, ts.sys, projectDirPath);
+
+  const program = ts.createProgram({ options, rootNames: fileNames, configFileParsingDiagnostics: errors });
+
+  const { diagnostics, emitSkipped } = program.emit();
+
+  const allDiagnostics = [...ts.getPreEmitDiagnostics(program), ...diagnostics, ...errors];
+
+  if (allDiagnostics.length > 0) {
+    const formatHost: ts.FormatDiagnosticsHost = {
+      getCanonicalFileName: (path) => path,
+      getCurrentDirectory: ts.sys.getCurrentDirectory,
+      getNewLine: () => ts.sys.newLine,
+    };
+    const message = ts.formatDiagnostics(allDiagnostics, formatHost);
+    console.warn(message);
+  }
+
+  process.exitCode = emitSkipped ? 1 : 0;
+}
