afa3a3aba8a5f1df4014db352ebb0a1864b8d965
Sakamoto, Kazunori <exkazuu@gmail.com>
2023-10-24 12:00:45 +0900

    fix: build errors

--- src/typeScript.ts
+++ src/typeScript.ts
@@ -1,19 +1,13 @@
-import type { FormatDiagnosticsHost } from 'typescript';
-import {
-  createProgram,
-  findConfigFile,
-  formatDiagnostics,
-  getPreEmitDiagnostics,
-  parseJsonConfigFileContent,
-  readConfigFile,
-  sys,
-} from 'typescript';
+/* eslint-disable import/no-named-as-default-member */
+
+// We cannot use named imports from 'typescript' because of build errors.
+import ts from 'typescript';
 
 export function generateDeclarationFiles(projectDirPath: string): boolean {
-  const configFile = findConfigFile(projectDirPath, sys.fileExists);
+  const configFile = ts.findConfigFile(projectDirPath, ts.sys.fileExists);
   if (!configFile) throw new Error(`Failed to find tsconfig.json in ${projectDirPath}.`);
 
-  const { config } = readConfigFile(configFile, sys.readFile);
+  const { config } = ts.readConfigFile(configFile, ts.sys.readFile);
   config.compilerOptions = {
     ...config.compilerOptions,
     declaration: true,
@@ -23,19 +17,19 @@ export function generateDeclarationFiles(projectDirPath: string): boolean {
     outDir: 'dist',
   };
   config.include = ['src/**/*'];
-  const { errors, fileNames, options } = parseJsonConfigFileContent(config, sys, projectDirPath);
+  const { errors, fileNames, options } = ts.parseJsonConfigFileContent(config, ts.sys, projectDirPath);
 
-  const program = createProgram({ options, rootNames: fileNames, configFileParsingDiagnostics: errors });
+  const program = ts.createProgram({ options, rootNames: fileNames, configFileParsingDiagnostics: errors });
   const { diagnostics, emitSkipped } = program.emit();
 
-  const allDiagnostics = [...getPreEmitDiagnostics(program), ...diagnostics, ...errors];
+  const allDiagnostics = [...ts.getPreEmitDiagnostics(program), ...diagnostics, ...errors];
   if (allDiagnostics.length > 0) {
-    const formatHost: FormatDiagnosticsHost = {
+    const formatHost: ts.FormatDiagnosticsHost = {
       getCanonicalFileName: (path) => path,
-      getCurrentDirectory: sys.getCurrentDirectory,
-      getNewLine: () => sys.newLine,
+      getCurrentDirectory: ts.sys.getCurrentDirectory,
+      getNewLine: () => ts.sys.newLine,
     };
-    const message = formatDiagnostics(allDiagnostics, formatHost);
+    const message = ts.formatDiagnostics(allDiagnostics, formatHost);
     console.warn(message);
   }
 
