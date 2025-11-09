a42be371da7ff0bbdd647b4fb7886437d7c55125
Sakamoto, Kazunori <exKAZUu@users.noreply.github.com>
2025-11-09 10:18:59 +0900

    fix: downgrade rollup (#429)

--- test/fixtures/lib-react/src/index.tsx
+++ test/fixtures/lib-react/src/index.tsx
@@ -2,5 +2,6 @@
 import chunk from 'lodash.chunk';
 
 export function Component() {
+  console.info(String.raw`\update`); // cf. https://github.com/rollup/rollup/issues/6175
   return <div>{JSON.stringify(chunk(['a', 'b', 'c', 'd'], 2))}</div>;
 }
